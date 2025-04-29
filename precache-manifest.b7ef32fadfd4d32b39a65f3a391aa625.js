8642805e9111a1fe4cf620dec8f88df2bd4b85c5
DimitarGeorgievski <muzipuzi123@gmail.com>
2025-04-29 22:43:34 +0200

    Updates

---
+++ precache-manifest.b7ef32fadfd4d32b39a65f3a391aa625.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc7a4df300b96f72b797fbac25c4da3e",
+    "url": "/React-students-grade-managment-system/index.html"
+  },
+  {
+    "revision": "f9797c685746a3e1353f",
+    "url": "/React-students-grade-managment-system/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/React-students-grade-managment-system/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "f9797c685746a3e1353f",
+    "url": "/React-students-grade-managment-system/static/js/main.11a0f281.chunk.js"
+  },
+  {
+    "revision": "baf0bf57ea89ea8c4fcf",
+    "url": "/React-students-grade-managment-system/static/js/runtime~main.dcd1d185.js"
+  }
+]);
